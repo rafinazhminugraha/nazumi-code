4f1ef8bf3d45038bfa62d592c02869664a7f2675
rafinazhminugraha <rafinazhminugraha@upi.edu>
2023-11-16 06:55:28 +0700

    nyobain rekayasa func

--- semester1/P9/9.5.py
+++ semester1/P9/9.5.py
@@ -1,4 +1,5 @@
-def selisih(jam_mulai, menit_mulai, detik_mulai, jam_selesai, menit_selesai, detik_selesai):
+#def selisih(jam_mulai, menit_mulai, detik_mulai, jam_selesai, menit_selesai, detik_selesai):
+def selisih():
     selisih_jam = jam_selesai - jam_mulai
     selisih_menit = menit_selesai - menit_mulai
     selisih_detik = detik_selesai - detik_mulai
@@ -8,7 +9,7 @@ def selisih(jam_mulai, menit_mulai, detik_mulai, jam_selesai, menit_selesai, det
     if selisih_menit < 0:
         selisih_menit += 60
         selisih_jam -= 1
-    if selisih_detik < 0:
+    elif selisih_detik < 0:
         selisih_detik += 60
         selisih_menit -= 1
     
@@ -18,12 +19,12 @@ print("Input Mulai:")
 jam_mulai = int(input("jam: "))
 menit_mulai = int(input("Menit: "))
 detik_mulai = int(input("detik: "))
-
+print("                               ")
 print("Input Selesai:")
 jam_selesai = int(input("jam: "))
 menit_selesai = int(input("Menit: "))
 detik_selesai = int(input("detik: "))
 
-selisih_jam, selisih_menit, selisih_detik = (selisih(jam_mulai, menit_mulai, detik_mulai, jam_selesai, menit_selesai, detik_selesai))
-    
-print(f"{selisih_jam} jam - {selisih_menit} menit - {selisih_detik} detik")
+selisih_j, selisih_m, selisih_d = selisih()
+
+print(f"{selisih_j} jam - {selisih_m} menit - {selisih_d} detik")
